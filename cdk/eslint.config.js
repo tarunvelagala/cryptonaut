88d56111d527f2b6ff2e42f4a282621d3c16b47b
Tarun Velagala <tarun.kumar.reddy.velagala@sap.com>
2025-12-07 11:22:06 +0530

    feat: introduce ESLint with TypeScript and Prettier configuration for code quality and formatting.

---
+++ cdk/eslint.config.js
@@ -0,0 +1,15 @@
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const prettier = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    prettier,
+    {
+        rules: {
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+            'no-console': 'warn',
+        },
+    },
+);
