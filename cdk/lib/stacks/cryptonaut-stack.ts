9a81374cd2878261694d771b4c5247858f387f9d
Tarun Velagala <tarun.kumar.reddy.velagala@sap.com>
2025-12-07 11:58:20 +0530

    chore: remove unused DynamoDB imports

--- cdk/lib/stacks/cryptonaut-stack.ts
+++ cdk/lib/stacks/cryptonaut-stack.ts
@@ -1,5 +1,4 @@
 import * as cdk from 'aws-cdk-lib';
-import { Table, AttributeType, BillingMode, TableEncryption } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class CryptonautStack extends cdk.Stack {
